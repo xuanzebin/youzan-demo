874e16fb82820969bbfbe3de9d690067ec1518a6
xuanzebin <xuanzebin@126.com>
2018-08-23 17:41:47 +0800

    给hot-goods添加了mint-ui库

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,20 +3,42 @@ import 'css/common.css'
 
 import url from 'js/api.js'
 import axios from 'axios'
-import Vue from 'vue';
+import Vue from 'vue'
+
+
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
 
 let view=new Vue({
     el:'#app',
     data:{
-        lists:null
+        lists:null,
+        pageNum:1,
+        pageSize:6,
+        loading:false,
+        allLoad:false
     },
     created(){
-        axios.post(url.hostLists,{
-            pageNum:1,
-            pageSize:6
-        }).then((response)=>{
-            console.log(response.data)
-            this.lists=response.data.lists
-        })
+        this.getList()
+    },
+    methods:{
+        getList(){
+            if (this.allLoad) return
+            this.loading=true
+            axios.post(url.hostLists,{
+                pageNum:this.pageNum,
+                pageSize:this.pageSize
+            }).then((response)=>{
+                let currentList=response.data.lists
+                if (currentList.length<this.pageSize) this.allLoad=true
+                if (this.lists) {
+                    this.lists=this.lists.concat(currentList)
+                } else {
+                    this.lists=currentList
+                }
+                this.pageNum++
+                this.loading=false
+            })
+        }
     }
 })
