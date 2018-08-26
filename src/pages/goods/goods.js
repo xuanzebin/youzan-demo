b7dc4f2ff13e8a225dabf0443d94ae1031c41f28
xuanzebin <xuanzebin@126.com>
2018-08-27 01:03:44 +0800

    完成了商品详情页的商品详情以及本店成交的部分

--- src/pages/goods/goods.js
+++ src/pages/goods/goods.js
@@ -12,13 +12,20 @@ import url from 'js/api.js'
 import qs from 'qs'
 import mixin from 'js/mixin.js'
 
+import Swipe from 'components/Swipe.vue'
+
 let {id}=qs.parse(window.location.search.substring(1))
+let detailTags=['商品详情','本店成交']
 
 let view=new Vue({
     el:'#app',
     data:{
         id,
-        detailsLists:null
+        detailsLists:null,
+        dealLists:null,
+        bannerLists:null,
+        detailTags,
+        curIndex:0
     },
     created(){
         this.getDetails()
@@ -26,8 +33,30 @@ let view=new Vue({
     methods:{
         getDetails(){
             axios.post(url.details,{id}).then((response)=>{
-                this.detailsLists=response.data
+                this.detailsLists=response.data.data
+                this.bannerLists=[]
+                this.detailsLists.imgs.forEach((value)=>{
+                    this.bannerLists.push({
+                        img:value,
+                        clickUrl:'javascript:;'
+                    })
+                })
+            })
+        },
+        chageCurIndex(index){
+            this.curIndex=index
+            if (index){
+                this.getDeal()
+            }
+        },
+        getDeal(){
+            axios.post(url.deal,{id}).then((response)=>{
+                this.dealLists=response.data.data.lists
             })
         }
-    }
+    },
+    components:{
+        Swipe
+    },
+    mixins:[mixin]
 })
