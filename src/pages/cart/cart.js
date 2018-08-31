7cf2671f257af0ffb16304086cfa92319fadf38b
xuanzebin <xuanzebin@126.com>
2018-08-31 13:48:52 +0800

    新增了拖拽删除的功能

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -6,6 +6,7 @@ import Vue from 'vue'
 import url from 'js/api.js'
 import mixin from 'js/mixin.js'
 import axios from 'axios'
+import Velocity from 'velocity-animate'
 
 let view=new Vue({
     el:'#app',
@@ -109,6 +110,7 @@ let view=new Vue({
                     shop.goodsList.forEach(good=>{
                         good.checked=true
                         good.removeChecked=false
+                        good.touchDelete=false
                     })
                 })
                 this.cartLists=list
@@ -137,6 +139,14 @@ let view=new Vue({
         },
         editingSwitch(shop,shopIndex){
             shop.editingStatus=!shop.editingStatus
+            if (shop.editingStatus){
+                shop.goodsList.forEach((good,index)=>{
+                    if (good.touchDelete){
+                        good.touchDelete=false
+                        Velocity(this.$refs[`goods-${shopIndex}-${index}`],{left:'0px'})
+                    }
+                })
+            }
             shop.editingMsg=shop.editingStatus?'完成':'编辑'
             this.editingShop=shop.editingStatus?shop:null
             this.editingShopIndex=shop.editingStatus?shopIndex:-1
@@ -167,7 +177,7 @@ let view=new Vue({
                 this.editingShop.goodsList.forEach(good=>{
                     if (good.removeChecked) ids.push(good.id)
                 })
-                axios.post(url.mrremove,{id:ids}).then(response=>{
+                axios.post(url.mrremove,{ids}).then(response=>{
                     let arr=[]
                     this.editingShop.goodsList.forEach(good=>{
                         if (!good.removeChecked) arr.push(good)
@@ -194,15 +204,37 @@ let view=new Vue({
             this.removeMsg=`确认删除所选的${arr.length}个商品?`
         },
         addGood(good,goodIndex){
-            axios.post(url.add,{id:good.id}).then(response=>{
+            axios.post(url.add,{id:good.id, number:1}).then(response=>{
                 good.number++
             })
         },
         reduceGood(good,goodIndex){
             if (good.number===1) return
-            axios.post(url.reduce,{id:good.id}).then(response=>{
+            axios.post(url.reduce,{id:good.id, number:-1}).then(response=>{
                 good.number--
             })
+        },
+        start(e,good){
+            good.startX=e.changedTouches[0].clientX
+        },
+        end(e,good,goodIndex,shopIndex,shop){
+            let endX=e.changedTouches[0].clientX
+            let left='0px'
+            if (good.startX-endX>100){
+                good.touchDelete=true
+                left='-60px'
+                Velocity(this.$refs[`goods-${shopIndex}-${goodIndex}`],{left})
+                shop.goodsList.forEach((otherGood,index)=>{
+                    if (otherGood.touchDelete && index!==goodIndex) {
+                        otherGood.touchDelete=false
+                        Velocity(this.$refs[`goods-${shopIndex}-${index}`],{left:'0px'})
+                    }
+                })
+            } else if (endX-good.startX>100) {
+                good.touchDelete=false
+                left='0px'
+                Velocity(this.$refs[`goods-${shopIndex}-${goodIndex}`],{left})
+            }
         }
     },
     mixins:[mixin]
